8b4d2e63d02d371f13c73539a8c19f03b05ae111
Park Seongheon <n30gu1@n30gu1.com>
2021-10-29 19:48:25 +0900

    Initial Commit

---
+++ Equati0n/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Equati0n
+//
+//  Created by 박성헌 on 2021/10/29.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
